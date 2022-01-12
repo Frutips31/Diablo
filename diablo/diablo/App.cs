9b67cf50eadf06cac85c1f33ed32ee9e490d913c
DESKTOP-V9QNVHB\Fru <soss0207>
2022-01-12 20:06:36 +0900

    최초 프로젝트 생성

---
+++ diablo/diablo/App.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace diablo
+{
+    class App
+    {
+       public App()
+        {
+            Console.WriteLine("hello world");
+        }
+    }
+}
